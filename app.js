57e2cb4daf1f0f417ea6e76e01592abce248b510
Faz-R <FazaR92@gmail.com>
2024-12-10 10:28:42 +0300

    fix: add alt for delete image in js file according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button button__delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className = 'delete__img';
+    deleteButtonImg.alt = ' ';
     deleteButton.appendChild(deleteButtonImg);
 
 
